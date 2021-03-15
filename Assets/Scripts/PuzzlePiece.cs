b898b5aafeb17edb0c47611f7b2f7906b8d8d6cc
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2021-03-16 00:15:38 +0100

    fixed direction bug in puzzlepiecescript

--- Assets/Scripts/PuzzlePiece.cs
+++ Assets/Scripts/PuzzlePiece.cs
@@ -6,14 +6,24 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Rigidbody))]
 public class PuzzlePiece : MonoBehaviour
 {
+    [SerializeField] float moveStrength;
+    [SerializeField] Direction moveDirection = Direction.X;
+
+    enum Direction
+    {
+        X,
+        Y
+    };
     public int nmbr = 1; //Has to be public
 
     float deltaForce;
     Vector3 mouseDelta;
     Vector3 lastMousePos;
+    Vector3 offset;
     Rigidbody rb;
-
+    Transform parentTransform;
     ParticleSystem particle;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +31,9 @@ public class PuzzlePiece : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         particle = GetComponentInChildren<ParticleSystem>();
         particle.gameObject.SetActive(false);
+        moveStrength = 2f;
+        parentTransform = GetComponentInParent<PuzzleManager>().transform;
+        lastMousePos = Input.mousePosition;
     }
 
     void Update()
@@ -28,18 +41,31 @@ public class PuzzlePiece : MonoBehaviour
         mouseDelta = Input.mousePosition - lastMousePos;
         lastMousePos = Input.mousePosition;
 
-
-
     }
-
 
     void OnMouseDrag() //called continuously while the button is pressed down every frame -----needs to fix the Algo to use all directions
     {
-        //Vector3 dir = Camera.main.ScreenToWorldPoint(mouseDelta);
+        Vector3 dir = parentTransform.position;
+        Vector3 newdir = Vector3.zero;
 
-        rb.AddForce(mouseDelta * Time.deltaTime);
+        switch (moveDirection)
+        {
+            case Direction.X:
+                float changeX = -mouseDelta.x;
+                 newdir = new Vector3(dir.x * changeX, 0f, 0f);
+                break;
 
+            case Direction.Y:
+                float changeY = mouseDelta.y * 10f;
+                 newdir = new Vector3(0f, dir.y * changeY, 0f);
+                break;
+        }
+
+        dir = parentTransform.TransformDirection(newdir);
+        rb.AddForce(dir * Time.deltaTime * moveStrength);
     }
+
+
 
     public void FitInPlace()
     {
